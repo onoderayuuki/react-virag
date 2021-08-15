257fe2d7314799f71840185e302aa1e66cfc97f6
onoderayuuki <38471145+onoderayuuki@users.noreply.github.com>
2021-08-15 18:01:30 +0900

    sampleコード削除

--- pages/sample-query.js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router"
-
-export default function AboutPage() {
-  const router = useRouter()
-  const {
-    query: { id },
-  } = router
-  return <div>About us: {id}</div>
-}

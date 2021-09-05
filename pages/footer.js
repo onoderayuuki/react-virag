afe5c674ce4d2dd7b7951cb22e74d9b8b8c3c7fc
onoderayuuki <38471145+onoderayuuki@users.noreply.github.com>
2021-09-05 12:39:54 +0900

    モチーフ一覧画面のツールバー下固定 #9

--- pages/footer.js
+++ pages/footer.js
@@ -2,25 +2,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 
+
+const useStyles = makeStyles({
+  appBar: {
+    top: 'auto',
+    bottom: 0,
+  }
+});
+
 export default function footer({ children }) {
-  const classes = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-    footer: {
-      position:'absolute',
-      top: "auto",
-      bottom: 0,
-    },
-  }));
+  const classes = useStyles();
     return (
-      <AppBar position="static" >
+      <AppBar position="fixed" className={classes.appBar}>
         <Toolbar variant="dense">
           {children}
         </Toolbar>

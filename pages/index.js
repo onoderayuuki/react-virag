5baeaebd1e96c9aad6fbfd53d155d20fb9545671
onoderayuuki <38471145+onoderayuuki@users.noreply.github.com>
2022-04-20 20:36:22 +0900

    atochi

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Typography from "@material-ui/core/Typography";
 export default function Index() {
   return (
     <Typography variant="h6" style={{ flexGrow: 1 }}>
-        ハンガリー毛糸刺繍 Virág
+        ハンガリー毛糸刺繍 Virág 跡地
     </Typography>
   )
 }
